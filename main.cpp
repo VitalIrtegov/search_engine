66fb4a7ab75058dc9b1d3e524aef92e61c9ca56e
Vital <v89824411104@yandex.ru>
2023-04-26 20:47:21 +0500

    added boost tokenizer2

--- main.cpp
+++ main.cpp
@@ -4,4 +4,9 @@
 int main() {
     Search search;
     search.setSearch();
+    std::set<std::string> set = search.getSearch();
+
+    for (std::string item : set) {
+        std::cout << item << std::endl;
+    }
 }
--- src/searchFiles.cpp
+++ src/searchFiles.cpp
@@ -13,14 +13,14 @@ void Search::setSearch() {
     if (in.is_open()) {
         //std::cout << "read" << std::endl;
         typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
-        boost::char_separator<char> sep("-;|, :\"\'.");
+        boost::char_separator<char> sep("-);|(, :\"\'./[]!_?><%");
 
         while (getline(in, line)) {
             tokenizer tokens(line, sep);
             for (tokenizer::iterator tok_iter = tokens.begin(); tok_iter != tokens.end(); ++tok_iter) {
-                std::cout << "<" << *tok_iter << "> ";
+                words.insert(*tok_iter);
+                //std::cout << "<" << *tok_iter << "> ";
             }
-            //std::cout << line << std::endl;
         }
     }
 
@@ -28,6 +28,5 @@ void Search::setSearch() {
 }
 
 std::set<std::string> Search::getSearch() {
-    //std::set<std::string> temp;
     return words;
 }
