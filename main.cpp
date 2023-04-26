715824230db44a1b493a3bd6e2bbb9f6c7005138
Vital <v89824411104@yandex.ru>
2023-04-26 20:38:47 +0500

    added boost tokenizer

--- main.cpp
+++ main.cpp
@@ -4,5 +4,4 @@
 int main() {
     Search search;
     search.setSearch();
-
 }
--- src/searchFiles.cpp
+++ src/searchFiles.cpp
@@ -2,18 +2,25 @@
 #include "../include/searchFiles.h"
 #include <iostream>
 #include <fstream>
+#include <boost/tokenizer.hpp>
 
 void Search::setSearch() {
     std::ifstream in("C:/drugs/beta.txt");
 
-    std::cout << "open" << std::endl;
+    //std::cout << "open" << std::endl;
     std::string line;
 
     if (in.is_open()) {
-        std::cout << "read" << std::endl;
-        while (getline(in, line))
-        {
-            std::cout << line << std::endl;
+        //std::cout << "read" << std::endl;
+        typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
+        boost::char_separator<char> sep("-;|, :\"\'.");
+
+        while (getline(in, line)) {
+            tokenizer tokens(line, sep);
+            for (tokenizer::iterator tok_iter = tokens.begin(); tok_iter != tokens.end(); ++tok_iter) {
+                std::cout << "<" << *tok_iter << "> ";
+            }
+            //std::cout << line << std::endl;
         }
     }
 
